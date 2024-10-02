33a051097ed42fec7a179ca35dc3e93c7a968d0a
Jay Nielsen <robots@nlsn.xyz>
2024-10-02 17:14:44 -0600

    docs: added intro anchor and heading docs (#17)
    * docs: added intro anchor and heading docs
    
    * docs: fix

---
+++ .github/CONTRIBUTING.md
@@ -0,0 +1,13 @@
+# Contributing
+
+Thanks for your interest in contributing to `@gaze-ui`! 💖
+
+> After this page, see [DEVELOPMENT.md](./DEVELOPMENT.md) for local development instructions.
+
+## Code of Conduct
+
+This project contains a [Contributor Covenant code of conduct](./CODE_OF_CONDUCT.md) all contributors are expected to follow.
+
+## Reporting Issues
+
+Please do [report an issue on the issue tracker](https://github.com/studio-drishti/gaze/issues/new/choose) if there's any bugfix, documentation improvement, or general enhancement you'd like to see in the repository! Please fully fill out all required fields in the most appropriate issue form.
--- packages/react/src/components/anchor/anchor.tsx
+++ packages/react/src/components/anchor/anchor.tsx
@@ -7,14 +7,14 @@ const cx = classNames.bind(styles);
 
 export type AnchorProps = React.ComponentPropsWithRef<"a">;
 
-export const Anchor = React.forwardRef<HTMLAnchorElement, AnchorProps>(
-  ({ children, className, ...props }, ref) => {
-    return (
-      <a className={cx("anchor", className)} ref={ref} {...props}>
-        {children}
-      </a>
-    );
-  },
-);
-
-Anchor.displayName = "Anchor";
+/** Links from one page to another. */
+export const Anchor = React.forwardRef(function Anchor(
+  { children, className, ...props }: AnchorProps,
+  ref: React.ForwardedRef<HTMLAnchorElement>,
+) {
+  return (
+    <a className={cx("anchor", className)} ref={ref} {...props}>
+      {children}
+    </a>
+  );
+});

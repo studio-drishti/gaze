73c27e5f94cb07caabc180c466e92c7add31b42b
Jay Nielsen <robots@nlsn.xyz>
2024-10-02 23:09:21 +0000

    docs: added intro anchor and heading docs

--- packages/react/src/components/heading/heading.tsx
+++ packages/react/src/components/heading/heading.tsx
@@ -8,22 +8,24 @@ const cx = classNames.bind(styles);
 type HeadingLevelOptions = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 
 export interface HeadingProps extends React.ComponentPropsWithRef<"h1"> {
+  /** The underlying HTML heading to render. */
   as: HeadingLevelOptions;
+  /** The heading level that the component should visually appear to be. Defaults to `as` when blank. */
   variant?: HeadingLevelOptions;
 }
 
-export const Heading = React.forwardRef<HTMLHeadingElement, HeadingProps>(
-  ({ as: HeadingComponent, children, className, variant }, ref) => {
-    const headingVariant = variant ?? HeadingComponent;
-    return (
-      <HeadingComponent
-        className={cx("heading", headingVariant, className)}
-        ref={ref}
-      >
-        {children}
-      </HeadingComponent>
-    );
-  },
-);
-
-Heading.displayName = "Heading";
+/** h1-h6 text that provides page hierarchy. */
+export const Heading = React.forwardRef(function Heading(
+  { as: HeadingComponent, children, className, variant }: HeadingProps,
+  ref: React.ForwardedRef<HTMLHeadingElement>,
+) {
+  const headingVariant = variant ?? HeadingComponent;
+  return (
+    <HeadingComponent
+      className={cx("heading", headingVariant, className)}
+      ref={ref}
+    >
+      {children}
+    </HeadingComponent>
+  );
+});
